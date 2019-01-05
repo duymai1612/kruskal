e2ea4eb0dd4e8ab17da7b21f006f6239d6754a7f
Duy Mai <duymai1612@gmail.com>
2019-01-05 11:44:47 +0700

    update graph logic

--- demo.js
+++ demo.js
@@ -2,11 +2,15 @@ var edgeList = [];
   const tbody = document.getElementById('table-body');
   const addBtn = document.getElementById('add-btn');
   const createBtn = document.getElementById('create-btn');
-  const submit = document.getElementById('submit');
   const nodeNum = document.getElementById('nodes-num');
   const node1 = document.getElementById('node-1');
   const node2 = document.getElementById('node-2');
   const weight = document.getElementById('weight');
+
+  const createWrapper = document.getElementById('create-wrapper');
+  const contentWrapper = document.getElementById('content-wrapper');
+
+  var g, gFinal;
   
   function updateTable() {
     edgeList.push({
@@ -25,96 +29,121 @@ var edgeList = [];
     `);
   }
 
+  function updateGraph() {
+    gFinal = g;
+    var kruskal = new jsgraphs.KruskalMST(g); 
+    var mst = kruskal.mst;
+    
+
+    var g_nodes = [];
+    var g_edges = [];
+    for (var v=0; v < g.V; ++v){
+      g.node(v).label = v; // assigned 'Node {v}' as label for node v
+      g_nodes.push({
+          id: v,
+          label: g.node(v).label
+      });
+    }
+    
+    for (var i=0; i < mst.length; ++i) {
+      var e = mst[i];
+      var v = e.either();
+      var w = e.other(v);
+      e.highlighted = true;
+      console.log('(' + v + ', ' + w + '): ' + e.weight);
+      g_edges.push({
+          from: v,
+          to: w,
+          length: e.weight,
+          label: '' + e.weight,
+          color: '#ff0000',
+          value: 2
+      });
+    }
+    
+    for (var v = 0; v < g.V; ++v) {
+      var adj_v = g.adj(v);
+      for (var i = 0; i < adj_v.length; ++i) {
+          var e = adj_v[i];
+          var w = e.other(v);
+          if (w > v) continue; // make sure only one edge between w and v since the graph is undirected
+          if (e.highlighted) continue;
+
+          g_edges.push({
+              from: v,
+              to: w,
+              length: e.weight,
+              label: '' + e.weight
+          });
+      };
+    }
+
+    console.log(g.V); // display the number of vertices
+    console.log(g.adj(0)); // display the adjacency list to vertex 0
+    
+    var nodes = new vis.DataSet(g_nodes);
+
+    // create an array with edges
+    var edges = new vis.DataSet(g_edges);
+
+    // create a network
+    var container = document.getElementById('mynetwork');
+    var data = {
+      nodes: nodes,
+      edges: edges
+    };
+    var options = {};
+    var network = new vis.Network(container, data, options);
+  }
+
   (function(){
 
-    var g, gFinal;
     nodeNum.addEventListener('keyup', (e) => {
       if (e.keyCode === 13) {
         g = new jsgraphs.WeightedGraph(nodeNum.value);
         alert(`Create graph with ${nodeNum.value} nodes successfully`);
+        createWrapper.style.display = 'none';
+        contentWrapper.style.display = 'flex';
+        updateGraph();
       }
     })
     createBtn.addEventListener('click', () => {
       g = new jsgraphs.WeightedGraph(nodeNum.value);
       alert(`Create graph with ${nodeNum.value} nodes successfully`);
+      createWrapper.style.display = 'none';
+      contentWrapper.style.display = 'flex';
+      updateGraph();
     });
 
+    node1.addEventListener('keyup', (e) => {
+      if (e.keyCode === 13) {
+        g.addEdge(new jsgraphs.Edge(node1.value, node2.value, weight.value));
+        updateTable();
+
+        updateGraph();
+      }
+    });
+    node2.addEventListener('keyup', (e) => {
+      if (e.keyCode === 13) {
+        g.addEdge(new jsgraphs.Edge(node1.value, node2.value, weight.value));
+        updateTable();
+
+        updateGraph();
+      }
+    });
     weight.addEventListener('keyup', (e) => {
       if (e.keyCode === 13) {
         g.addEdge(new jsgraphs.Edge(node1.value, node2.value, weight.value));
         updateTable();
+
+        updateGraph();
       }
-    })
+    });
     addBtn.addEventListener('click', () => {
       g.addEdge(new jsgraphs.Edge(node1.value, node2.value, weight.value));
       updateTable();
+
+      updateGraph();
     });
-
-    submit.addEventListener('click', function() {
-      gFinal = g;
-      var kruskal = new jsgraphs.KruskalMST(g); 
-      var mst = kruskal.mst;
-      
-
-      var g_nodes = [];
-      var g_edges = [];
-      for (var v=0; v < g.V; ++v){
-        g.node(v).label = v; // assigned 'Node {v}' as label for node v
-        g_nodes.push({
-            id: v,
-            label: g.node(v).label
-        });
-      }
-      
-      for (var i=0; i < mst.length; ++i) {
-        var e = mst[i];
-        var v = e.either();
-        var w = e.other(v);
-        e.highlighted = true;
-        console.log('(' + v + ', ' + w + '): ' + e.weight);
-        g_edges.push({
-            from: v,
-            to: w,
-            length: e.weight,
-            label: '' + e.weight,
-            color: '#ff0000',
-            value: 2
-        });
-      }
-      
-      for (var v = 0; v < g.V; ++v) {
-        var adj_v = g.adj(v);
-        for (var i = 0; i < adj_v.length; ++i) {
-            var e = adj_v[i];
-            var w = e.other(v);
-            if (w > v) continue; // make sure only one edge between w and v since the graph is undirected
-            if (e.highlighted) continue;
-
-            g_edges.push({
-                from: v,
-                to: w,
-                length: e.weight,
-                label: '' + e.weight
-            });
-        };
-      }
-
-      console.log(g.V); // display the number of vertices
-      console.log(g.adj(0)); // display the adjacency list to vertex 0
-      
-      var nodes = new vis.DataSet(g_nodes);
-
-      // create an array with edges
-      var edges = new vis.DataSet(g_edges);
-
-      // create a network
-      var container = document.getElementById('mynetwork');
-      var data = {
-        nodes: nodes,
-        edges: edges
-      };
-      var options = {};
-      var network = new vis.Network(container, data, options);
-    })
 
   })();
